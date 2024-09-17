01a0b3895d1ea5b75aa695b59fe2857c72d44d16
Rishabh Pomaje <97334879+RishP11@users.noreply.github.com>
2024-09-17 21:32:52 +0530

    removed extra #defines + cleanup

--- Group02_Part01/main.c
+++ Group02_Part01/main.c
@@ -1,13 +1,5 @@
-// #define STCTRL *((volatile long *) 0xE000E010)          // control and status
-// #define STRELOAD *((volatile long *) 0xE000E014)        // reload value
-// #define STCURRENT *((volatile long *) 0xE000E018)       // current value
-
-// #define COUNT_FLAG  (1 << 16)                           // bit 16 of CSR automatically set to 1
-// #define ENABLE      (1 << 0)                            // bit 0 of CSR to enable the timer
-// #define CLKINT      (1 << 2)                            // bit 2 of CSR to specify CPU clock
 #define CLOCK_HZ    16000000                            // Timer clock frequency
-#define MAX_RELOAD  16777215
-#define PWM_FREQ    100000                                  // Frequency of the PWM waveform in Hz
+#define PWM_FREQ    100000                              // Frequency of the PWM waveform in Hz
 float duty_cycle = 50.0 ;
 
 #include <stdint.h>
