95d15f077ad7d3dd2defba16594c44af256f2deb
Rishabh Pomaje <97334879+RishP11@users.noreply.github.com>
2024-09-18 07:45:06 +0530

    refactored code; removed an edge case bug.

--- Group02_Part02/main.c
+++ Group02_Part02/main.c
@@ -1,6 +1,6 @@
 #define CLOCK_HZ    16000000                            // Timer clock frequency
 #define PWM_FREQ    100000                              // Frequency of the PWM waveform in Hz
-float duty_cycle = 50.0 ;
+float duty_cycle = 50.0 ;                               // On-time Duty cycle of the PWM waveform
 
 #include <stdint.h>
 #include <stdbool.h>
@@ -20,30 +20,30 @@ int main(void)
     // Setup Interrupt for GPIO
     GPIOF_SETUP();
     while(1){
-        ;
+        ;// Handled by interrupts & ISR
     }
 }
 
 void PORT_F_init( void )
 {
-    SYSCTL_RCGC2_R |= SYSCTL_RCGC2_GPIOF ;      // Enable clock to GPIO_F
-    GPIO_PORTF_LOCK_R = 0x4C4F434B ;            // Unlock commit register
-    GPIO_PORTF_CR_R = 0x1F ;                    // Make PORTF0 configurable
-    GPIO_PORTF_DEN_R = 0x1F ;                   // Set PORTF pins 4 pin
-    GPIO_PORTF_DIR_R = 0x0E ;                   // Set PORTF4 pin as input user switch pin
-    GPIO_PORTF_PUR_R = 0x11 ;                   // Set the switches for Pull Up
-    GPIO_PORTF_AFSEL_R |= (1 << 1) ;                 // Alternate function assignment
-    GPIO_PORTF_PCTL_R |= (1 << 6) | (1 << 5) | (1 << 4) ; // Assign T0CCP1
+    SYSCTL_RCGC2_R |= SYSCTL_RCGC2_GPIOF ;                  // Enable clock to GPIO_F
+    GPIO_PORTF_LOCK_R = 0x4C4F434B ;                        // Unlock commit register
+    GPIO_PORTF_CR_R = 0x1F ;                                // Make PORTF0 configurable
+    GPIO_PORTF_DEN_R = 0x1F ;                               // Set PORTF pins 4 pin
+    GPIO_PORTF_DIR_R = 0x0E ;                               // Set PORTF4 pin as input user switch pin
+    GPIO_PORTF_PUR_R = 0x11 ;                               // Set the switches for Pull Up
+    GPIO_PORTF_AFSEL_R |= (1 << 1) ;                        // Alternate function assignment
+    GPIO_PORTF_PCTL_R |= (1 << 6) | (1 << 5) | (1 << 4) ;   // Assign T0CCP1
 }
 
 void GPTM_SETUP( float duty_cycle ){
     unsigned long int count_value = CLOCK_HZ * (1.0 / PWM_FREQ) ;
     unsigned long int match_value = count_value - (duty_cycle * CLOCK_HZ / (100 * PWM_FREQ)) ;
-    // Provide clock access to GPTM
+    // Enable clock to GPTM
     SYSCTL_RCGCTIMER_R |= 0x01 ;
-    // GPTM Control Register :: Ensuring the Timer is OFF
+    // GPTM Control Register :: Ensure the Timer is OFF
     TIMER0_CTL_R = 0x00 ;
-    // GPTM Configuration register :: Single mode
+    // GPTM Configuration register :: Single (register) mode
     TIMER0_CFG_R = 0x04 ;
     // GPTM Timer and Mode Register :: PWM Mode
     TIMER0_TBMR_R = 0x00 ;
@@ -71,8 +71,7 @@ void GPIOF_SETUP( void )
     // Interrupt Both Edges Register :: Used to configure whether both edges are to be detected as events for interrupt.
     GPIO_PORTF_IBE_R = 0x00 ;
     // Interrupt Mask Register :: To determine whether to allow the interrupt generated to be passed onto interrupt controller or not.
-    // 1 == Send the interrupt
-    // 0 == Do not send the interrupt
+    // 1 => Send the interrupt and 0 => Do not send the interrupt
     GPIO_PORTF_IM_R = 0x11 ;
     // Interrupt Clear Register :: Used to clear any pending interrupts
     GPIO_PORTF_ICR_R = 0x11 ;
@@ -82,20 +81,24 @@ void GPIOF_SETUP( void )
 void GPIOF_ISR( void )
 {
     // PORT F = ...|SW1|G|B|R|SW2|
-    GPIO_PORTF_ICR_R = 0x11 ;
-    int cnt = 0;
-    while(cnt<1000000){
-        cnt = cnt + 1;
+    int hold_time = 0;
+    int threshold = 1000000 ;
+    while(hold_time < threshold){
+        hold_time += 1;
     }
-    if((~(GPIO_PORTF_DATA_R) & 0x11) && duty_cycle < 95 && duty_cycle > 5){
-        duty_cycle -= 5.0 ;
+    if ((~(GPIO_PORTF_DATA_R) & 0x11) && (duty_cycle >= 5)) {
+        // Long press ==> Decrease
+        duty_cycle -= 4.99 ;
     }
     else{
-        duty_cycle += 5.0 ;
+        // Short press ==> Increase
+        if (duty_cycle <= 95){
+            duty_cycle += 4.99 ;
     }
+    GPIO_PORTF_ICR_R = 0x11 ;
     unsigned long int count_value = CLOCK_HZ * (1.0 / PWM_FREQ) ;
     unsigned long int match_value = count_value - (duty_cycle * CLOCK_HZ / (100 * PWM_FREQ)) ;
-    // GPTM Interval Load Register :: Count Top value
-    // GPTM Match Value register
+    // Match Value Update
     TIMER0_TBMATCHR_R = match_value ;
+    }
 }
